896456eed581d1066f015b002d76fe1a557c2998
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-08 12:28:39 +0530

    Update upload_logs_to_s3.py

--- upload_logs_to_s3.py
+++ upload_logs_to_s3.py
@@ -2,5 +2,7 @@
 
 0 8 * * * /bin/python2.7 /home/tsmadmin/scripts/upload_s3_logs.py >> /home/tsmadmin/scripts/cron_log.txt 2>&1
 
+s3://gbt-tableaubucket/logs/_var_opt_tableau_tableau_server_data_tabsvc_logs_httpd/
+
 
 
