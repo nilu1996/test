b7a4ea8e9f1646f2411676d443364c60398d273d
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-08 11:57:45 +0530

    Update upload_logs_to_s3.py

--- upload_logs_to_s3.py
+++ upload_logs_to_s3.py
@@ -1,5 +1,2 @@
-  File "test.py", line 72, in <module>
-    zip_file_path = create_zip_folder(local_logs_path, old_logs)
-  File "test.py", line 12, in create_zip_folder
-    with zipfile.ZipFile(zip_file_name, 'w') as zipf:
-NameError: global name 'zipfile' is not defined
+ 0 8 * * * /path/to/log_backup_to_s3.py
+
