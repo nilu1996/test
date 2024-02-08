1b528bdda4bc97f2328509ed0fc499aa35cff53d
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-08 12:07:52 +0530

    Update upload_logs_to_s3.py

--- upload_logs_to_s3.py
+++ upload_logs_to_s3.py
@@ -1,3 +1,6 @@
  0 8 * * * /bin/python2.7 /home/tsmadmin/scripts/upload_s3_logs.py
 
+0 8 * * * /bin/python2.7 /home/tsmadmin/scripts/upload_s3_logs.py >> /home/tsmadmin/scripts/cron_log.txt 2>&1
+
+
 
