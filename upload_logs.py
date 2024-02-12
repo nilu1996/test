33353fff2188d7cd948dccc92b4c98798ca985a6
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-12 15:20:21 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,89 +1,37 @@
-import os
 import boto3
-from botocore.exceptions import NoCredentialsError
-from datetime import datetime, timedelta
-import shutil
-import zipfile
 
-def create_zip_folder(local_path, files):
-    """
-    Creates a zip file for the specified directory.
-    """
-    zip_file_name = local_path + ".zip"
-    with zipfile.ZipFile(zip_file_name, 'w') as zipf:
-        for file in files:
-            zipf.write(os.path.join(local_path, file), file)
-    return zip_file_name
-
-def upload_to_s3(zip_file_path, bucket_name, s3_base_path, local_path):
-    """
-    Uploads a zip file to an S3 bucket with a timestamped folder using instance profile credentials.
-    """
-    s3 = boto3.client('s3')
-
-    try:
-        # Create timestamped folder
-        timestamped_folder = datetime.now().strftime("%Y%m%d_%H%M%S")
-        s3_path = os.path.join(s3_base_path, local_path.replace(os.path.sep, '_'), timestamped_folder)
-
-        # Upload the zip file to S3
-        s3_file_path = os.path.join(s3_path, os.path.basename(zip_file_path))
-        s3.upload_file(zip_file_path, bucket_name, s3_file_path)
-
-        print("Uploaded {} to {}/{}".format(zip_file_path, bucket_name, s3_file_path))
-
-        print("Upload from {} to {} successful.".format(zip_file_path, s3_path))
-        return True
-    except NoCredentialsError:
-        print("Credentials not available.")
-        return False
-
-def collect_logs_older_than_seven_days(logs_dir):
-    """
-    Collects log files older than seven days from the specified directory.
-    """
-    logs = []
-    seven_days_ago = datetime.now() - timedelta(days=7)
-    for file_name in os.listdir(logs_dir):
-        file_path = os.path.join(logs_dir, file_name)
-        modified_time = datetime.fromtimestamp(os.path.getmtime(file_path))
-        if modified_time < seven_days_ago:
-            logs.append(file_name)
-    return logs
-
-# Specify your local logs folder paths
-local_logs_paths = [
-    "/var/opt/tableau/tableau_server/logs",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/httpd",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/collections",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/httpd",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/vizportal",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/webhooks",
-    "/var/opt/tableau/tableau_server/data/tabsvc/logs/dataserver"
-]
-
-# Specify your S3 bucket name
-s3_bucket_name = "gbt-tableaubucket"
-
-# Specify the base S3 path where logs should be uploaded
-s3_upload_base_path = "logs"
-
-# Upload logs from each local path
-for local_logs_path in local_logs_paths:
-    # Collect log files older than seven days
-    old_logs = collect_logs_older_than_seven_days(local_logs_path)
-
-    # Create a zip file for old logs
-    if old_logs:
-        zip_file_path = create_zip_folder(local_logs_path, old_logs)
-
-        # Upload the zip file to S3
-        if upload_to_s3(zip_file_path, s3_bucket_name, s3_upload_base_path, local_logs_path):
-            # Remove old logs
-            for log_file in old_logs:
-                os.remove(os.path.join(local_logs_path, log_file))
-            print("Old logs from {} moved to S3 successfully.".format(local_logs_path))
-        else:
-            print("Failed to move old logs from {} to S3.".format(local_logs_path))
-    else:
-        print("No old logs found in {}.".format(local_logs_path))
+def lambda_handler(event, context):
+    # Create EC2 client
+    ec2_client = boto3.client('ec2')
+    sns_client = boto3.client('sns')
+    
+    # Get all instances in the region
+    instances = ec2_client.describe_instances()
+    
+    # Initialize a list to store instances with status other than 'running'
+    instances_not_running = []
+    
+    # Check each instance's status
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            state = instance['State']['Name']
+            # Check if instance state is not 'running'
+            if state != 'running':
+                instances_not_running.append(instance_id)
+    
+    # If there are instances not running, trigger an email
+    if instances_not_running:
+        # Create SNS client
+        sns_client = boto3.client('sns')
+        
+        # Set up the email subject and message
+        subject = "EC2 Instance(s) Not Running"
+        message = f"The following EC2 instance(s) are not running: {', '.join(instances_not_running)}"
+        
+        # Publish the message to the specified SNS topic
+        sns_client.publish(
+            TopicArn='arn:aws:sns:us-east-1:211125567787:test-lambda',
+            Message=message,
+            Subject=subject
+        )
