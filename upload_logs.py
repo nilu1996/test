7dac6051969b303c54bd0bbd8d0528ed2fbaed8c
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-14 17:18:28 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -2,12 +2,12 @@ import urllib.request
 import boto3
 import time
 
-def send_sns_notification(topic_arn, message, region_name):
-    sns_client = boto3.client('sns', region_name=region_name)
+def send_sns_notification(topic_arn, message):
+    sns_client = boto3.client('sns', verify=False)
     sns_client.publish(TopicArn=topic_arn, Message=message)
     print("SNS notification sent successfully.")
 
-def check_website_availability(url, sns_topic_arn, region_name):
+def check_website_availability(url, sns_topic_arn):
     try:
         response = urllib.request.urlopen(url)
         status_code = response.getcode()
@@ -15,15 +15,14 @@ def check_website_availability(url, sns_topic_arn, region_name):
             print("Website is available.")
         else:
             message = f"The website {url} is unavailable. Status code: {status_code}"
-            send_sns_notification(sns_topic_arn, message, region_name)
+            send_sns_notification(sns_topic_arn, message)
     except Exception as e:
         message = f"Failed to connect to the website {url}: {e}"
-        send_sns_notification(sns_topic_arn, message, region_name)
+        send_sns_notification(sns_topic_arn, message)
 
 if __name__ == "__main__":
     website_url = "https://devtableau.gbt.gbtad.com/"
-    sns_topic_arn = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"  
-    region_name = "us-east-1"  # Replace with your desired AWS region
+    sns_topic_arn = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
     while True:
-        check_website_availability(website_url, sns_topic_arn, region_name)
+        check_website_availability(website_url, sns_topic_arn)
         time.sleep(1800)
