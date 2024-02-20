3e5006a7c607f8632a04f6a98e27d98cc4e14053
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-20 14:19:26 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run Tableau backup command
+tsm maintenance backup -d
+
+# Move backup file to S3 bucket
+aws s3 cp /var/opt/tableau/tableau_server/data/tabsvc/files/backups/*.tsbak s3://your-s3-bucket/
+
+# Remove old backup files
+# Example: Remove files older than 7 days
+find /var/opt/tableau/tableau_server/data/tabsvc/files/backups/ -type f -name "*.tsbak" -mtime +7 -exec rm {} \;
