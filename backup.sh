2ee25339594bdda28643db6ee9b44655c7ab74f3
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-20 15:19:36 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 tsm maintenance backup -d
 
 # Move backup file to S3 bucket
-aws s3 cp /var/opt/tableau/tableau_server/data/tabsvc/files/backups/*.tsbak s3://your-s3-bucket/
+aws s3 cp /var/opt/tableau/tableau_server/data/tabsvc/files/backups/*.tsbak s3://gbt-tableaubucket/
 
 # Remove old backup files
 # Example: Remove files older than 7 days
