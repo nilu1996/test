7b6c4d67b4f368af8718c02a3a6bef23ed08be44
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-20 15:25:32 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Define paths
+BACKUP_DIR="/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
+S3_BUCKET="s3://gbt-tableaubucket/"
+
 # Run Tableau backup command
 tsm maintenance backup -d
 
-# Move backup file to S3 bucket
-aws s3 cp /var/opt/tableau/tableau_server/data/tabsvc/files/backups/*.tsbak s3://gbt-tableaubucket/
-
-# Remove old backup files
-# Example: Remove files older than 7 days
-find /var/opt/tableau/tableau_server/data/tabsvc/files/backups/ -type f -name "*.tsbak" -mtime +7 -exec rm {} \;
+# Check if backup files exist
+if [ -n "$(find "$BACKUP_DIR" -maxdepth 1 -type f -name "*.tsbak" -print -quit)" ]; then
+    # Move backup file to S3 bucket
+    aws s3 cp "$BACKUP_DIR"*.tsbak "$S3_BUCKET"
+    # Remove old backup files
+    # Example: Remove files older than 7 days
+    find "$BACKUP_DIR" -maxdepth 1 -type f -name "*.tsbak" -mtime +7 -exec rm {} \;
+else
+    echo "No backup files found in $BACKUP_DIR"
+fi
