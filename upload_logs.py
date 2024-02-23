f3f8126327a8f29500cfc21419944bbf134aeece
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-02-23 15:07:33 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,89 +1,29 @@
-import os
+import urllib.request
 import boto3
-from botocore.exceptions import NoCredentialsError
-from datetime import datetime, timedelta
-import shutil
-import zipfile
+import time
 
-def create_zip_folder(local_path, files):
-    """
-    Creates a zip file for the specified directory.
-    """
-    zip_file_name = local_path + ".zip"
-    with zipfile.ZipFile(zip_file_name, 'w', allowZip64=True) as zipf:
-        for file in files:
-            zipf.write(os.path.join(local_path, file), file)
-    return zip_file_name
+def send_sns_notification(topic_arn, message, region_name):
+    sns_client = boto3.client('sns', region_name=region_name, verify=False)
+    sns_client.publish(TopicArn=topic_arn, Message=message)
+    print("SNS notification sent successfully.")
 
-def upload_to_s3(zip_file_path, bucket_name, s3_base_path, local_path):
-    """
-    Uploads a zip file to an S3 bucket with a timestamped folder using instance profile credentials.
-    """
-    s3 = boto3.client('s3')
-
+def check_website_availability(url, sns_topic_arn, region_name):
     try:
-        # Create timestamped folder
-        timestamped_folder = datetime.now().strftime("%Y%m%d_%H%M%S")
-        s3_path = os.path.join(s3_base_path, local_path.replace(os.path.sep, '_'), timestamped_folder)
-
-        # Upload the zip file to S3
-        s3_file_path = os.path.join(s3_path, os.path.basename(zip_file_path))
-        s3.upload_file(zip_file_path, bucket_name, s3_file_path)
-
-        print("Uploaded {} to {}/{}".format(zip_file_path, bucket_name, s3_file_path))
-
-        print("Upload from {} to {} successful.".format(zip_file_path, s3_path))
-        return True
-    except NoCredentialsError:
-        print("Credentials not available.")
-        return False
-
-def collect_logs_older_than_seven_days(logs_dir):
-    """
-    Collects log files older than seven days from the specified directory.
-    """
-    logs = []
-    seven_days_ago = datetime.now() - timedelta(days=7)
-    for file_name in os.listdir(logs_dir):
-        file_path = os.path.join(logs_dir, file_name)
-        # Check if the current item is a file and not a directory
-        if os.path.isfile(file_path):
-            modified_time = datetime.fromtimestamp(os.path.getmtime(file_path))
-            if modified_time < seven_days_ago:
-                logs.append(file_name)
-    return logs
-
-# Specify your local logs folder path
-local_logs_root = "/var/opt/tableau/tableau_server/data/tabsvc/logs/"
-
-# Specify your S3 bucket name
-s3_bucket_name = "gbt-tableaubucket"
-
-# Specify the base S3 path where logs should be uploaded
-s3_upload_base_path = "logs"
-
-# Iterate over subdirectories within the logs group folder
-for log_group_folder in os.listdir(local_logs_root):
-    local_logs_path = os.path.join(local_logs_root, log_group_folder)
-    
-    # Check if the path is a directory
-    if os.path.isdir(local_logs_path):
-        # Collect log files older than seven days
-        old_logs = collect_logs_older_than_seven_days(local_logs_path)
-
-        # Create a zip file for old logs
-        if old_logs:
-            zip_file_path = create_zip_folder(local_logs_path, old_logs)
-
-            # Upload the zip file to S3
-            if upload_to_s3(zip_file_path, s3_bucket_name, s3_upload_base_path, local_logs_path):
-                # Remove old logs
-                for log_file in old_logs:
-                    os.remove(os.path.join(local_logs_path, log_file))
-                print("Old logs from {} moved to S3 successfully.".format(local_logs_path))
-            else:
-                print("Failed to move old logs from {} to S3.".format(local_logs_path))
+        response = urllib.request.urlopen(url)
+        status_code = response.getcode()
+        if status_code == 200:
+            print("Website is available.")
         else:
-            print("No old logs found in {}.".format(local_logs_path))
-    else:
-        print("{} is not a directory.".format(local_logs_path))
+            message = f"The website {url} is unavailable. Status code: {status_code}"
+            send_sns_notification(sns_topic_arn, message, region_name)
+    except Exception as e:
+        message = f"Failed to connect to the website {url}: {e}"
+        send_sns_notification(sns_topic_arn, message, region_name)
+
+if __name__ == "__main__":
+    website_url = "https://devtableau.gbt.gbtad.com/"
+    sns_topic_arn = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
+    region_name = "us-east-1"  # Replace with your desired AWS region
+    while True:
+        check_website_availability(website_url, sns_topic_arn, region_name)
+        time.sleep(1800)
