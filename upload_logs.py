b406ea84b3d1363ecfbbfbf3916ecf7fb5912f59
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-03-04 15:56:21 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,62 +1,8 @@
-import os
-import datetime
-import boto3
-
-# Set the default region for boto3 globally
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-
-# Define paths
-BACKUP_DIR = "/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
-S3_BUCKET = "gbt-uattableau"
-S3_PREFIX = "backup/"
-SNS_ARN = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
-EMAIL_SUBJECT = "Tableau Server Backup Notification"
-
-# Run Tableau backup command
-backup_file = "backup.tsbak"
-backup_command = "tsm maintenance backup -f {} -d".format(backup_file)
-os.system(backup_command)
-
-# Check if backup file exists
-if os.path.isfile(os.path.join(BACKUP_DIR, backup_file)):
-    # Move backup file to specific location
-    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    new_backup_file = "backup_{}.tsbak".format(backup_time)
-    os.rename(os.path.join(BACKUP_DIR, backup_file), os.path.join(BACKUP_DIR, new_backup_file))
-    
-    # Remove older backup files
-    for file in os.listdir(BACKUP_DIR):
-        if file.startswith("backup_") and file.endswith(".tsbak"):
-            file_time = datetime.datetime.strptime(file[7:-6], "%Y%m%d%H%M%S")
-            if (datetime.datetime.now() - file_time) > datetime.timedelta(days=7):
-                os.remove(os.path.join(BACKUP_DIR, file))
-
-    try:
-        # Move backup file to S3 bucket
-        s3_client = boto3.client("s3")
-        s3_key = S3_PREFIX + new_backup_file
-        s3_client.upload_file(os.path.join(BACKUP_DIR, new_backup_file), S3_BUCKET, s3_key)
-
-        # Send success notification
-        sns_client = boto3.client("sns")
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup completed successfully."
-        )
-    except Exception as e:
-        # Send failure notification
-        sns_client = boto3.client("sns")
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup failed. Error: {}".format(str(e))
-        )
-else:
-    # Send failure notification (backup file not found)
-    sns_client = boto3.client("sns")
-    sns_client.publish(
-        TopicArn=SNS_ARN,
-        Subject=EMAIL_SUBJECT,
-        Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR)
-    )
+Traceback (most recent call last):
+  File "back.py", line 58, in <module>
+    sns_client = boto3.client("sns", config=botocore.config.Config(verify=False))  # Disable SSL verification
+  File "/usr/lib/python2.7/site-packages/botocore/config.py", line 193, in __init__
+    args, kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/config.py", line 220, in _record_user_provided_options
+    'Got unexpected keyword argument \'%s\'' % key)
+TypeError: Got unexpected keyword argument 'verify'
