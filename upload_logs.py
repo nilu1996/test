e98e12dcf40f526b5a6698d9032a1797f9bfda19
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-03-04 16:04:27 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,35 +1,40 @@
-  warnings.warn(warning, PythonDeprecationWarning)
-Traceback (most recent call last):
-  File "backyp.py", line 62, in <module>
-    Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR)
-  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 386, in _api_call
-    return self._make_api_call(operation_name, kwargs)
-  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 692, in _make_api_call
-    operation_model, request_dict, request_context)
-  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 711, in _make_request
-    return self._endpoint.make_request(operation_model, request_dict)
-  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 102, in make_request
-    return self._send_request(request_dict, operation_model)
-  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 137, in _send_request
-    success_response, exception):
-  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 256, in _needs_retry
-    caught_exception=caught_exception, request_dict=request_dict)
-  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 356, in emit
-    return self._emitter.emit(aliased_event_name, **kwargs)
-  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 228, in emit
-    return self._emit(event_name, kwargs)
-  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 211, in _emit
-    response = handler(**kwargs)
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 183, in __call__
-    if self._checker(attempts, response, caught_exception):
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 251, in __call__
-    caught_exception)
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 277, in _should_retry
-    return self._checker(attempt_number, response, caught_exception)
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 317, in __call__
-    caught_exception)
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 223, in __call__
-    attempt_number, caught_exception)
-  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 359, in _check_caught_exception
-    raise caught_exception
-botocore.exceptions.SSLError: SSL validation failed for https://sns.us-east-1.amazonaws.com/ [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:727)
+import os
+import subprocess
+import boto3
+import botocore
+
+# Define paths
+BACKUP_DIR = "/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
+S3_BUCKET = "s3://gbt-uattableau/backup/"
+SNS_ARN = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
+EMAIL_SUBJECT = "Tableau Server Backup Notification"
+
+# Run Tableau backup command
+backup_file = "backup-2024-03-04.tsbak"
+subprocess.run(["tsm", "maintenance", "backup", "-f", backup_file, "-d"], cwd=BACKUP_DIR)
+
+# Check if backup files exist
+if os.path.exists(os.path.join(BACKUP_DIR, backup_file)):
+    # Move backup file to S3 bucket
+    s3_key = "{}/{}".format(S3_BUCKET, backup_file)
+    s3_client = boto3.client("s3", config=botocore.client.Config(signature_version=botocore.UNSIGNED))
+    s3_client.upload_file(os.path.join(BACKUP_DIR, backup_file), s3_key)
+
+    # Remove old backup files (Example: Remove files older than 7 days)
+    # Implement your logic here
+
+    # Send success notification
+    sns_client = boto3.client("sns", config=botocore.client.Config(signature_version=botocore.UNSIGNED))
+    sns_client.publish(
+        TopicArn=SNS_ARN,
+        Subject=EMAIL_SUBJECT,
+        Message="Tableau UAT Server backup completed successfully.",
+    )
+else:
+    # Send failure notification
+    sns_client = boto3.client("sns", config=botocore.client.Config(signature_version=botocore.UNSIGNED))
+    sns_client.publish(
+        TopicArn=SNS_ARN,
+        Subject=EMAIL_SUBJECT,
+        Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR),
+    )
