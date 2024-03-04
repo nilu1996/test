c55b6f7d39ad020e1e659f70196767e02b348c27
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-03-04 15:44:38 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,63 +1,35 @@
-import os
-import datetime
-import boto3
-from botocore.exceptions import NoCredentialsError
-
-# Set the default region for boto3 globally
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-
-# Define paths
-BACKUP_DIR = "/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
-S3_BUCKET = "gbt-uattableau"
-S3_PREFIX = "backup/"
-SNS_ARN = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
-EMAIL_SUBJECT = "Tableau Server Backup Notification"
-
-# Run Tableau backup command
-backup_file = "backup.tsbak"
-backup_command = "tsm maintenance backup -f {} -d".format(backup_file)
-os.system(backup_command)
-
-# Check if backup file exists
-if os.path.isfile(os.path.join(BACKUP_DIR, backup_file)):
-    # Move backup file to specific location
-    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    new_backup_file = "backup_{}.tsbak".format(backup_time)
-    os.rename(os.path.join(BACKUP_DIR, backup_file), os.path.join(BACKUP_DIR, new_backup_file))
-    
-    # Remove older backup files
-    for file in os.listdir(BACKUP_DIR):
-        if file.startswith("backup_") and file.endswith(".tsbak"):
-            file_time = datetime.datetime.strptime(file[7:-6], "%Y%m%d%H%M%S")
-            if (datetime.datetime.now() - file_time) > datetime.timedelta(days=7):
-                os.remove(os.path.join(BACKUP_DIR, file))
-
-    try:
-        # Move backup file to S3 bucket
-        s3_client = boto3.client("s3")  # No need to specify region here
-        s3_key = S3_PREFIX + new_backup_file
-        s3_client.upload_file(os.path.join(BACKUP_DIR, new_backup_file), S3_BUCKET, s3_key)
-
-        # Send success notification
-        sns_client = boto3.client("sns")  # No need to specify region here
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup completed successfully."
-        )
-    except NoCredentialsError:
-        # Send failure notification (no AWS credentials)
-        sns_client = boto3.client("sns")  # No need to specify region here
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup failed. No AWS credentials found."
-        )
-else:
-    # Send failure notification (backup file not found)
-    sns_client = boto3.client("sns")  # No need to specify region here
-    sns_client.publish(
-        TopicArn=SNS_ARN,
-        Subject=EMAIL_SUBJECT,
-        Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR)
-    )
+  warnings.warn(warning, PythonDeprecationWarning)
+Traceback (most recent call last):
+  File "bac.py", line 62, in <module>
+    Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 386, in _api_call
+    return self._make_api_call(operation_name, kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 692, in _make_api_call
+    operation_model, request_dict, request_context)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 711, in _make_request
+    return self._endpoint.make_request(operation_model, request_dict)
+  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 102, in make_request
+    return self._send_request(request_dict, operation_model)
+  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 137, in _send_request
+    success_response, exception):
+  File "/usr/lib/python2.7/site-packages/botocore/endpoint.py", line 256, in _needs_retry
+    caught_exception=caught_exception, request_dict=request_dict)
+  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 356, in emit
+    return self._emitter.emit(aliased_event_name, **kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 228, in emit
+    return self._emit(event_name, kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/hooks.py", line 211, in _emit
+    response = handler(**kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 183, in __call__
+    if self._checker(attempts, response, caught_exception):
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 251, in __call__
+    caught_exception)
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 277, in _should_retry
+    return self._checker(attempt_number, response, caught_exception)
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 317, in __call__
+    caught_exception)
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 223, in __call__
+    attempt_number, caught_exception)
+  File "/usr/lib/python2.7/site-packages/botocore/retryhandler.py", line 359, in _check_caught_exception
+    raise caught_exception
+botocore.exceptions.SSLError: SSL validation failed for https://sns.us-east-1.amazonaws.com/ [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:727)
