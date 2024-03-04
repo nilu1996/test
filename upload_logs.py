8a53bb1934a76126e39665bb421f41ee85fbd4e9
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-03-04 15:33:36 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,61 +1,28 @@
-import os
-import datetime
-import boto3
-from botocore.exceptions import NoCredentialsError
-
-# Define paths
-BACKUP_DIR = "/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
-S3_BUCKET = "gbt-uattableau"
-S3_PREFIX = "backup/"
-S3_REGION = "us-east-1"  # Specify the region here
-SNS_ARN = "arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
-EMAIL_SUBJECT = "Tableau Server Backup Notification"
-
-# Run Tableau backup command
-backup_file = "backup.tsbak"
-backup_command = "tsm maintenance backup -f {} -d".format(backup_file)
-os.system(backup_command)
-
-# Check if backup file exists
-if os.path.isfile(os.path.join(BACKUP_DIR, backup_file)):
-    # Move backup file to specific location
-    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    new_backup_file = "backup_{}.tsbak".format(backup_time)
-    os.rename(os.path.join(BACKUP_DIR, backup_file), os.path.join(BACKUP_DIR, new_backup_file))
-    
-    # Remove older backup files
-    for file in os.listdir(BACKUP_DIR):
-        if file.startswith("backup_") and file.endswith(".tsbak"):
-            file_time = datetime.datetime.strptime(file[7:-6], "%Y%m%d%H%M%S")
-            if (datetime.datetime.now() - file_time) > datetime.timedelta(days=7):
-                os.remove(os.path.join(BACKUP_DIR, file))
-
-    try:
-        # Move backup file to S3 bucket
-        s3_client = boto3.client("s3", region_name=S3_REGION)  # Specify the region here
-        s3_key = S3_PREFIX + new_backup_file
-        s3_client.upload_file(os.path.join(BACKUP_DIR, new_backup_file), S3_BUCKET, s3_key)
-
-        # Send success notification
-        sns_client = boto3.client("sns")
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup completed successfully."
-        )
-    except NoCredentialsError:
-        # Send failure notification (no AWS credentials)
-        sns_client = boto3.client("sns")
-        sns_client.publish(
-            TopicArn=SNS_ARN,
-            Subject=EMAIL_SUBJECT,
-            Message="Tableau UAT Server backup failed. No AWS credentials found."
-        )
-else:
-    # Send failure notification (backup file not found)
+Traceback (most recent call last):
+  File "backup.py", line 56, in <module>
     sns_client = boto3.client("sns")
-    sns_client.publish(
-        TopicArn=SNS_ARN,
-        Subject=EMAIL_SUBJECT,
-        Message="Tableau UAT Server backup failed. Backup file {} not found in {}".format(backup_file, BACKUP_DIR)
-    )
+  File "/usr/lib/python2.7/site-packages/boto3/__init__.py", line 93, in client
+    return _get_default_session().client(*args, **kwargs)
+  File "/usr/lib/python2.7/site-packages/boto3/session.py", line 263, in client
+    aws_session_token=aws_session_token, config=config)
+  File "/usr/lib/python2.7/site-packages/botocore/session.py", line 851, in create_client
+    client_config=config, api_version=api_version)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 88, in create_client
+    verify, credentials, scoped_config, client_config, endpoint_bridge)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 357, in _get_client_args
+    verify, credentials, scoped_config, client_config, endpoint_bridge)
+  File "/usr/lib/python2.7/site-packages/botocore/args.py", line 73, in get_client_args
+    endpoint_url, is_secure, scoped_config)
+  File "/usr/lib/python2.7/site-packages/botocore/args.py", line 154, in compute_client_args
+    s3_config=s3_config,
+  File "/usr/lib/python2.7/site-packages/botocore/args.py", line 220, in _compute_endpoint_config
+    return self._resolve_endpoint(**resolve_endpoint_kwargs)
+  File "/usr/lib/python2.7/site-packages/botocore/args.py", line 303, in _resolve_endpoint
+    service_name, region_name, endpoint_url, is_secure)
+  File "/usr/lib/python2.7/site-packages/botocore/client.py", line 431, in resolve
+    service_name, region_name)
+  File "/usr/lib/python2.7/site-packages/botocore/regions.py", line 134, in construct_endpoint
+    partition, service_name, region_name)
+  File "/usr/lib/python2.7/site-packages/botocore/regions.py", line 148, in _endpoint_for_partition
+    raise NoRegionError()
+botocore.exceptions.NoRegionError: You must specify a region.
