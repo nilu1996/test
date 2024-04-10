cb3ee6b8f102605a32638b8116f7bc340a93f908
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-04-10 13:33:30 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,7 +1,35 @@
-{
-  "errorMessage": "'App'",
-  "errorType": "KeyError",
-  "stackTrace": [
-    "  File \"/var/task/index.py\", line 30, in lambda_handler\n    last_activity_timestamp = activity_status['App']['LastHealthCheckTimestamp']\n"
-  ]
-}
+import boto3
+import time
+
+sagemaker_client = boto3.client('sagemaker')
+
+IDLE_THRESHOLD_SECONDS = 300  # 5 minutes
+
+def lambda_handler(event, context):  
+    response = sagemaker_client.list_apps()
+    print(response)
+    for app in response['Apps']:
+        app_name = app['AppName']
+        app_type = app['AppType']
+        app_domain = app['DomainId']
+        app_spacename = app.get('SpaceName')  # Use get() method to handle KeyError
+        print(app_name)
+        activity_status = sagemaker_client.describe_app(
+            AppName=app_name,
+            AppType=app_type,
+            DomainId=app_domain,
+            SpaceName=app_spacename
+        )
+        print(activity_status)
+        if activity_status == 'Stopped':
+            print(f'Studio Lab instance "{app_name}" is already stopped.')
+        else:
+            last_activity_timestamp = activity_status['App']['LastHealthCheckTimestamp']
+            current_time = int(time.time())
+            idle_time = current_time - last_activity_timestamp
+            if idle_time >= IDLE_THRESHOLD_SECONDS:
+                sagemaker_client.stop_app(AppName=app_name)
+                print(f'Stopped Studio Lab instance "{app_name}" due to inactivity.')
+            else:
+                print(f'Studio Lab instance "{app_name}" is active, not yet past the idle threshold.')
+    return {'statusCode': 200, 'body': 'Success'}
