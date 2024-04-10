1b5fb9e899cb088ccb274034900fc1b4e2a9eb47
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-04-10 13:39:12 +0530

    Update upload_logs.py

--- upload_logs.py
+++ upload_logs.py
@@ -1,44 +1,9 @@
-import boto3
-import time
+{'AppArn': 'arn:aws:sagemaker:us-east-1:787098058239:app/d-ushbqegfkcgl/jupytertest/jupyterlab/default', 'AppType': 'JupyterLab', 'AppName': 'default', 'DomainId': 'd-ushbqegfkcgl', 'SpaceName': 'jupytertest', 'Status': 'InService', 'LastHealthCheckTimestamp': datetime.datetime(2024, 4, 10, 8, 7, 52, 973000, tzinfo=tzlocal()), 'LastUserActivityTimestamp': datetime.datetime(2024, 4, 10, 8, 7, 52, 973000, tzinfo=tzlocal()), 'CreationTime': datetime.datetime(2024, 4, 10, 7, 8, 57, 58000, tzinfo=tzlocal()), 'ResourceSpec': {'SageMakerImageArn': 'arn:aws:sagemaker:us-east-1:885854791233:image/sagemaker-distribution-cpu', 'SageMakerImageVersionAlias': '1.6.0', 'InstanceType': 'ml.t3.medium'}, 'ResponseMetadata': {'RequestId': '0fe59209-9f29-484b-b24c-cd537b22abe0', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': '0fe59209-9f29-484b-b24c-cd537b22abe0', 'content-type': 'application/x-amz-json-1.1', 'content-length': '645', 'date': 'Wed, 10 Apr 2024 08:08:29 GMT'}, 'RetryAttempts': 0}}
 
-sagemaker_client = boto3.client('sagemaker')
 
-IDLE_THRESHOLD_SECONDS = 300  # 5 minutes
-
-def lambda_handler(event, context):
-    response = sagemaker_client.list_apps()
-    print(response)
-    for app in response['Apps']:
-        app_name = app['AppName']
-        app_type = app['AppType']
-        app_domain = app['DomainId']
-        print(app_name)
-        
-        # Determine which attribute to use based on application type
-        if app_type == 'JupyterLab':
-            app_attribute = app['SpaceName']
-        elif app_type == 'JupyterServer':
-            app_attribute = app['UserProfileName']
-        else:
-            print(f"Unsupported application type: {app_type}")
-            continue
-        
-        activity_status = sagemaker_client.describe_app(
+activity_status = sagemaker_client.describe_app(
             AppName=app_name,
             AppType=app_type,
             DomainId=app_domain,
             SpaceName=app_attribute  # Use the determined attribute
         )
-        print(activity_status)
-        if activity_status == 'Stopped':
-            print(f'Studio Lab instance "{app_name}" is already stopped.')
-        else:
-            last_activity_timestamp = activity_status['App']['LastHealthCheckTimestamp']
-            current_time = int(time.time())
-            idle_time = current_time - last_activity_timestamp
-            if idle_time >= IDLE_THRESHOLD_SECONDS:
-                sagemaker_client.stop_app(AppName=app_name)
-                print(f'Stopped Studio Lab instance "{app_name}" due to inactivity.')
-            else:
-                print(f'Studio Lab instance "{app_name}" is active, not yet past the idle threshold.')
-    return {'statusCode': 200, 'body': 'Success'}
