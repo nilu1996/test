cbcab37b5d2593b902c48bf61f7b7b07fc4cb4dc
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-04-12 13:11:21 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,15 +1,13 @@
-Script is looking for tsm when running from cron tab and its getting failed due to that
-TSM loccation: /opt/tableau/tableau_server/packages/customer-bin.20233.23.1017.0948/tsm
-
-Keep script same change tsm backup command setting
+#!/bin/bash
 
 BACKUP_DIR="/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
 S3_BUCKET="s3://gbt-uattableau/backup/"
 SNS_ARN="arn:aws:sns:us-east-1:090124397890:Instance-Health-monitoring"
 EMAIL_SUBJECT="Tableau Server Backup Notification"
+TSM_LOCATION="/opt/tableau/tableau_server/packages/customer-bin.20233.23.1017.0948/tsm"
 
 # Run Tableau backup command
-tsm maintenance backup -f backup.tsbak -d
+"$TSM_LOCATION" maintenance backup -f backup.tsbak -d
 
 # Remove backup files older than 2 days
 find "$BACKUP_DIR" -maxdepth 1 -type f -name "*.tsbak" -mtime +2 -exec rm {} \;
