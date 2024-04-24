23d008c56250b35abe5f15f5e861a9c783b0eb42
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-04-24 11:35:20 +0530

    Update aws_transfer_family.py

--- aws_transfer_family.py
+++ aws_transfer_family.py
@@ -1,29 +1,27 @@
 import boto3
 import json
 
-# Replace 'your_server_id' with the 
-# actual ID of your Transfer server
-SERVER_ID = 'your_server_id'
+# Replace 'your_server_id' with the actual ID of your Transfer server
+SERVER_ID = 's-01234567890abcdef'
 
-def stop_transfer_server(server_id):
+def start_transfer_server(server_id):
     """
-    Stop an AWS Transfer Family server.
+    Start an AWS Transfer Family server.
 
     Parameters:
-    - server_id: The ID of the server to stop.
+    - server_id: The ID of the server to start.
     """
     # Initialize the Transfer client
     transfer_client = boto3.client('transfer')
 
-
-    response = transfer_client.stop_server(ServerId=server_id)
-    print("Server stopped successfully.")
+    response = transfer_client.start_server(ServerId=server_id)
+    print("Server started successfully.")
 
 def lambda_handler(event, context):
 
     try:
-        # Call the function to stop the server
-        stop_transfer_server(SERVER_ID)
+        # Call the function to start the server
+        start_transfer_server(SERVER_ID)
 
         return {
             "statusCode": 200,
@@ -31,7 +29,8 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "Server ": str(SERVER_ID)
+                "Server ": str(SERVER_ID),
+                "Status": "Started"
             })
         }
     except Exception as err:
@@ -41,6 +40,7 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "Server ": str(SERVER_ID)
+                "Server ": str(SERVER_ID),
+                "Error": str(err)
             })
         }
