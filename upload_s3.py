720af62c23bf4deb175d5066d599d92e820c6388
nilu1996 <156421587+nilu1996@users.noreply.github.com>
2024-05-21 12:23:44 +0530

    Update upload_s3.py

--- upload_s3.py
+++ upload_s3.py
@@ -1,58 +1,19 @@
-import os
 import boto3
-from botocore.exceptions import NoCredentialsError
-from datetime import datetime
 
-def upload_to_s3(local_path, bucket_name, s3_base_path):
-    """
-    Upload a directory to an S3 bucket with a timestamped folder using instance profile credentials.
-    """
-    s3 = boto3.client('s3')
+def lambda_handler(event, context):
+    # Define your EC2 region
+    ec2_region = 'us-east-1'
 
-    try:
-        # Create timestamped folder
-        timestamped_folder = datetime.now().strftime("%Y%m%d_%H%M%S")
-        s3_path = os.path.join(s3_base_path, timestamped_folder)
+    # Initialize the EC2 client
+    ec2_client = boto3.client('ec2', region_name=ec2_region)
 
-        # Upload files to S3
-        for root, dirs, files in os.walk(local_path):
-            for file in files:
-                local_file_path = os.path.join(root, file)
-                relative_path = os.path.relpath(local_file_path, local_path)
+    # List of instance IDs to start
+    instance_ids = ['i-0834b38f7628b7a6c']
 
-                # Create the folder in S3 if it doesn't exist
-                s3.put_object(Bucket=bucket_name, Key=os.path.join(s3_path, relative_path) + '/')
+    # Start each instance
+    for instance_id in instance_ids:
+        response = ec2_client.start_instances(InstanceIds=[instance_id])
+        print(response)
+        print(f"Started EC2 instance: {instance_id}")
 
-                # Add timestamp to the S3 file path
-                s3_file_path = os.path.join(s3_path, relative_path)
-
-                print("Uploading {} to {}/{}".format(local_file_path, bucket_name, s3_file_path))
-                s3.upload_file(local_file_path, bucket_name, s3_file_path)
-
-        print("Upload from {} to {} successful.".format(local_path, s3_path))
-        return True
-    except NoCredentialsError:
-        print("Credentials not available.")
-        return False
-
-# Specify your local logs folder paths
-local_logs_path_1 = "/var/opt/tableau/tableau_server/logs"
-local_logs_path_2 = "/var/opt/tableau/tableau_server/data/tabsvc/logs/httpd"
-
-# Specify your S3 bucket name
-s3_bucket_name = "gbt-tableaubucket"
-
-# Specify the base S3 path where logs should be uploaded
-s3_upload_base_path = "logs"
-
-# Upload logs from the first local path
-if upload_to_s3(local_logs_path_1, s3_bucket_name, os.path.join(s3_upload_base_path, 'var_opt')):
-    print("Logs from {} moved to S3 successfully.".format(local_logs_path_1))
-else:
-    print("Failed to move logs from {} to S3.".format(local_logs_path_1))
-
-# Upload logs from the second local path
-if upload_to_s3(local_logs_path_2, s3_bucket_name, os.path.join(s3_upload_base_path, 'var_backups')):
-    print("Logs from {} moved to S3 successfully.".format(local_logs_path_2))
-else:
-    print("Failed to move logs from {} to S3.".format(local_logs_path_2))
+    print("All specified EC2 instances have been started.")
